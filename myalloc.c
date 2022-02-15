da4a65c402745f890a362c02332514ccd189b36d
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-02-15 09:10:46 -0800

    Fixed issues caused by sbrk moving memory 130K bytes

--- myalloc.c
+++ myalloc.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #define ALIGNMENT 16   // Must be power of 2
-#define HEAP_SIZE 1024
+#define PAGE_SIZE 1024
 #define GET_PAD(x) ((ALIGNMENT - 1) - ((x) - 1) & (ALIGNMENT - 1))
 #define PADDED_SIZE(x) ((x) + GET_PAD(x))
 #define PTR_OFFSET(p, offset) ((void*)((char *)(p) + (offset)))
@@ -19,11 +19,10 @@ void *myalloc(int size){
     if (head == NULL){ // Initialize heap on first call
         
         printf("Init; Location of inital break point: %p\n", sbrk(0));
-        head = (struct block *) sbrk(HEAP_SIZE);
-        printf("Location of end break point: %p\n", sbrk(0));
-        printf("Initial head location: %p\n", head);
+        head = (struct block *) sbrk(PAGE_SIZE);
+        
         head->next = NULL;
-        head->size = HEAP_SIZE - PADDED_BLOCK_SIZE;
+        head->size = PAGE_SIZE - PADDED_BLOCK_SIZE;
         head->in_use = 0;
 
     }
@@ -41,7 +40,7 @@ void *myalloc(int size){
             int remaining_size = available_space - PADDED_SIZE(size);
             
             if(remaining_size >= PADDED_BLOCK_SIZE + ALIGNMENT){ // If there is leftover space in current data block for a new block AND more PADDED data, then we need to create a new node between the current node and its next node
-                printf("Creating intermediary node at end of current nodes data\n");
+                // printf("Creating intermediary node at end of current nodes data\n");
                 p->size = PADDED_SIZE(size);
                 struct block *next = p->next;
                 p->next = (struct block *) (((char*)(p + 1)) + PADDED_SIZE(size));// Start of next block = Address of start of current block + size of padded block + size of padded data
@@ -57,19 +56,22 @@ void *myalloc(int size){
         p = p->next;
     }while(p != NULL);
     // If last node is free, grow its size, if its used, make new node
-    printf("Could not find space to allocate %d bytes (%d with padding) in heap. Increasing heap size by %d\n", size, PADDED_SIZE(size), HEAP_SIZE);
+    printf("Could not find space to allocate %d bytes (%d with padding) in heap. Increasing heap size by %d\n", size, PADDED_SIZE(size), PAGE_SIZE);
     if(last->in_use == 1){ 
-        printf("Current end of break point %p\n", sbrk(0));
-        last->next = (struct block *) sbrk(HEAP_SIZE);
-        printf("New end of break point: %p\n", sbrk(0));
+        // printf("Current end of break point %p\n", sbrk(0));
+        
+        last->next = (struct block *) sbrk(PAGE_SIZE);
+        // printf("New end of break point: %p\n", sbrk(0));
+       
         last->next->size = ((char*)(((char*)sbrk(0)) - ((char*) last->next)) - ( (char*) PADDED_BLOCK_SIZE) );
         last->next->in_use = 0;
         print_data();
         
     }else{
-        printf("Current end of break point %p\n", sbrk(0));
-        sbrk(HEAP_SIZE); // Set new break point to the old break point + the HEAP_SIZE
-        printf("New end of break point: %p\n", sbrk(0));
+        // printf("Current end of break point %p\n", sbrk(0));
+        sbrk(PAGE_SIZE); // Set new break point to the old break point + the PAGE_SIZE
+        // printf("New end of break point: %p\n", sbrk(0));
+        
         last->size = (char*)sbrk(0) - (char*) (last + 1);
         print_data();
     }
@@ -77,7 +79,7 @@ void *myalloc(int size){
 }
 
 void myfree(void * p){
-    printf("Freeing block starting at %p\n\n", ((struct block *) p) - 1);
+    // printf("Freeing block starting at %p\n\n", ((struct block *) p) - 1);
     (((struct block *) p) - 1)->in_use = 0; // Set the block in use value to 0. The block is 1 block size behind the start of the data pointer
 }
 
@@ -106,12 +108,20 @@ void print_data(void)
 
 int main(void){
     
-    int *p = (int * ) myalloc(1008);
+    int *p = (int*) myalloc(100);
     print_data();
-    int *p1 = (int * ) myalloc(5000);
+    int *p1 = (int*) myalloc(500);
     print_data();
-    
-    
+    myfree(p);
+    print_data();
+    int *p2 = (int*) myalloc(5000);
+    print_data();
+    int *p3 = (int*) myalloc(5000);
+    print_data();
+    myfree(p2);
+    print_data();
+    myalloc(200);
+    print_data();
     
     return 0;
 }
