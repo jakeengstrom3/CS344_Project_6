127647b2473c9f4ee314bc8b0c78d1b1c4d518ec
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-02-15 13:46:16 -0800

    refacotred myalloc() to use helpr functions

--- myalloc.c
+++ myalloc.c
@@ -15,68 +15,97 @@ struct block{
 
 struct block *head = NULL;
 
-void *myalloc(int size){
-    if (head == NULL){ // Initialize heap on first call
-        
-        printf("Init; Location of inital break point: %p\n", sbrk(0));
-        head = (struct block *) sbrk(PAGE_SIZE);
-        
-        head->next = NULL;
-        head->size = PAGE_SIZE - PADDED_BLOCK_SIZE;
-        head->in_use = 0;
-
+int check_block_availability(struct block *p, int size){
+    if(p->in_use == 0 && (p->size >= PADDED_SIZE(size))){ // Use first block that is free, and has a enough space to accomodate the padded size, and a new block
+        return 1; // Free block with enough space
     }
+    return 0;
+}
+
+void split_space(struct block **p, int remaining_size, int size){
+    // printf("Creating intermediary node at end of current nodes data\n");
+    (*p)->size = PADDED_SIZE(size);
+    struct block *next = (*p)->next;
+    (*p)->next = (struct block *) (((char*)((*p) + 1)) + PADDED_SIZE(size));// Start of next block = Address of start of current block + size of padded block + size of padded data
+    (*p)->next->size = remaining_size - PADDED_BLOCK_SIZE; // Size of new node is the size of the old node - size of new data - size of a new node
+    (*p)->next->in_use = 0; // New block is not being used
+    (*p)->next->next = next; // New Block's next is the old blocks next block
+}
+
+void *find_space(int size){
 
     struct block *p = head;
     struct block *last;
-    do{
-        int available_space = p->size; 
-        if(p->in_use == 0 && (available_space >= PADDED_SIZE(size))){ // Use first block that is free, and has a enough space to accomodate the padded size, and a new block
-            // Found usable space
-            // printf("Address of start of block: %p\nAddress of start of data: %p\n", p,  p + 1);
 
+    for(struct block *p = head; p != NULL; p = p->next){
+         
+        if(check_block_availability(p, size) == 1){
             printf("Allocating %d bytes (%d with padding, %d with padding and padded block) in heap starting at address: %p.\n", size, PADDED_SIZE(size), (PADDED_SIZE(size) + PADDED_BLOCK_SIZE), p);
-            p->in_use = 1;
-            int remaining_size = available_space - PADDED_SIZE(size);
-            
-            if(remaining_size >= PADDED_BLOCK_SIZE + ALIGNMENT){ // If there is leftover space in current data block for a new block AND more PADDED data, then we need to create a new node between the current node and its next node
-                // printf("Creating intermediary node at end of current nodes data\n");
-                p->size = PADDED_SIZE(size);
-                struct block *next = p->next;
-                p->next = (struct block *) (((char*)(p + 1)) + PADDED_SIZE(size));// Start of next block = Address of start of current block + size of padded block + size of padded data
-                p->next->size = remaining_size - PADDED_BLOCK_SIZE; // Size of new node is the size of the old node - size of new data - size of a new node
-                p->next->in_use = 0; // New block is not being used
-                p->next->next = next; // New Block's next is the old blocks next block
+            int remaining_size = p->size - PADDED_SIZE(size);
+            if(remaining_size >= PADDED_BLOCK_SIZE + ALIGNMENT){
+                split_space(&p, remaining_size, size);
             }else{ 
                 printf("Not enough space in current block for intermediary node, Allocating %d bytes to full block starting at %p\n", size, p);
             }
-            return p + 1; // Return address to data
+            p->in_use = 1;
+            return p + 1; // Return address to data (not block)
         }
-        last = p;
+    }
+    return NULL;
+}
+
+void increase_heap_size(){
+    struct block *p = head;
+    
+    while(p->next != NULL){
         p = p->next;
-    }while(p != NULL);
-    // If last node is free, grow its size, if its used, make new node
-    printf("Could not find space to allocate %d bytes (%d with padding) in heap. Increasing heap size by %d\n", size, PADDED_SIZE(size), PAGE_SIZE);
-    if(last->in_use == 1){ 
+    }
+
+    if(p->in_use == 1){ 
         // printf("Current end of break point %p\n", sbrk(0));
-        
-        last->next = (struct block *) sbrk(PAGE_SIZE);
+        p->next = (struct block *) sbrk(PAGE_SIZE);
         // printf("New end of break point: %p\n", sbrk(0));
-       
-        last->next->size = ((char*)(((char*)sbrk(0)) - ((char*) last->next)) - ( (char*) PADDED_BLOCK_SIZE) );
-        last->next->in_use = 0;
+        p->next->size = ((char*)(((char*)sbrk(0)) - ((char*) p->next)) - ( (char*) PADDED_BLOCK_SIZE) );
+        p->next->in_use = 0;
         print_data();
         
     }else{
         // printf("Current end of break point %p\n", sbrk(0));
         sbrk(PAGE_SIZE); // Set new break point to the old break point + the PAGE_SIZE
         // printf("New end of break point: %p\n", sbrk(0));
-        
-        last->size = (char*)sbrk(0) - (char*) (last + 1);
+        p->size = (char*)sbrk(0) - (char*) (p + 1);
         print_data();
     }
-    myalloc(size);
+    
 }
+
+void initialize_heap(){ // Create tart of list, and intiialze all its values
+    printf("Initial Location of inital break point: %p\n", sbrk(0)); // For some reason this print statment is necessary to keep successive calls to brk from incrimenting the break point by 130K bytes
+    head = (struct block *) sbrk(PAGE_SIZE);
+    head->next = NULL;
+    head->size = PAGE_SIZE - PADDED_BLOCK_SIZE;
+    head->in_use = 0;
+
+}
+
+void *myalloc(int size){
+    if (head == NULL){ // Initialize heap on first call
+        initialize_heap();
+    }
+
+    struct block *p = (struct block *) find_space(size); // Returns pointer to available block of memory, or NULL if none available
+
+    while(p == NULL){ // Find space did not succeed. Increase heap size, and try again
+        printf("Could not find space to allocate %d bytes (%d with padding) in heap. Increasing heap size by %d\n", size, PADDED_SIZE(size), PAGE_SIZE);
+        increase_heap_size();
+        p = find_space(size);
+        // If last node is free, grow its size, if its used, make new node
+    }   
+
+    return p + 1; // Return pointer to data (instead of block)
+}
+
+
 
 void myfree(void * p){
     // printf("Freeing block starting at %p\n\n", ((struct block *) p) - 1);
@@ -108,21 +137,15 @@ void print_data(void)
 
 int main(void){
     
-    int *p = (int*) myalloc(100);
-    print_data();
-    int *p1 = (int*) myalloc(500);
-    print_data();
-    myfree(p);
-    print_data();
-    int *p2 = (int*) myalloc(5000);
-    print_data();
-    int *p3 = (int*) myalloc(5000);
-    print_data();
-    myfree(p2);
-    print_data();
-    myalloc(200);
-    print_data();
+    void *p;
+
+    myalloc(10);     print_data();
+    p = myalloc(20); print_data();
+    myalloc(30);     print_data();
+    myfree(p);       print_data();
+    myalloc(40);     print_data();
+    myalloc(10);     print_data();
+    myalloc(5000);   print_data();
     
     return 0;
 }
-
